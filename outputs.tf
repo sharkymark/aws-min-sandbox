e8375b3ae0e655431965ce6851119211aa0625f8
Mark Milligan <mark@nuon.co>
2025-08-02 14:43:34 -0500

    fix: update outputs for nuon dns

--- outputs.tf
+++ outputs.tf
@@ -48,30 +48,6 @@ output "nuon_dns" {
     enabled         = local.nuon_dns.enabled,
     public_domain   = local.nuon_dns.enabled ? module.nuon_dns[0].public_domain : { zone_id : "", name : "", nameservers : tolist([""]) }
     internal_domain = local.nuon_dns.enabled ? module.nuon_dns[0].internal_domain : { zone_id : "", name : "", nameservers : tolist([""]) }
-    alb_ingress_controller = {
-      enabled  = local.nuon_dns.enabled
-      id       = local.nuon_dns.enabled ? module.nuon_dns[0].alb_ingress_controller.release.id : ""
-      chart    = local.nuon_dns.enabled ? module.nuon_dns[0].alb_ingress_controller.release.chart : ""
-      revision = local.nuon_dns.enabled ? module.nuon_dns[0].alb_ingress_controller.release.revision : ""
-    }
-    external_dns = {
-      enabled  = local.nuon_dns.enabled
-      id       = local.nuon_dns.enabled ? module.nuon_dns[0].external_dns.release.id : ""
-      chart    = local.nuon_dns.enabled ? module.nuon_dns[0].external_dns.release.chart : ""
-      revision = local.nuon_dns.enabled ? module.nuon_dns[0].external_dns.release.revision : ""
-    }
-    cert_manager = {
-      enabled  = local.nuon_dns.enabled
-      id       = local.nuon_dns.enabled ? module.nuon_dns[0].cert_manager.release.id : ""
-      chart    = local.nuon_dns.enabled ? module.nuon_dns[0].cert_manager.release.chart : ""
-      revision = local.nuon_dns.enabled ? module.nuon_dns[0].cert_manager.release.revision : ""
-    }
-    ingress_nginx = {
-      enabled  = local.nuon_dns.enabled
-      id       = local.nuon_dns.enabled ? module.nuon_dns[0].ingress_nginx.release.id : ""
-      chart    = local.nuon_dns.enabled ? module.nuon_dns[0].ingress_nginx.release.chart : ""
-      revision = local.nuon_dns.enabled ? module.nuon_dns[0].ingress_nginx.release.revision : ""
-    }
   }
-  description = "A map of Nuon DNS attributes: whether nuon.run has been enabled; AWS Route 53 details for the public_domain and internal_domain; metadata bout the helm charts the module installs on."
+  description = "A map of Nuon DNS attributes: whether nuon.run has been enabled; AWS Route 53 details for the public_domain and internal_domain."
 }
